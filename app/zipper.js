14e8927603505b9881ee1c5ad7d4dff4a21b096f
Austin Gordon <gordon.austin2@gmail.com>
2018-03-26 10:28:43 -0500

    Added filter to only zip ready files

--- app/zipper.js
+++ app/zipper.js
@@ -5,111 +5,152 @@ exports.run = function() {
 
     return new Promise(function(resolve, reject){
 
-        console.log('Starting zip process.');
-
         var i = 0;
-        
-        fs.readdir('./claim-data/', (err, files) => {
+        var filesReady = [];
+
+        getClaims().then((claims) => {
+    
+            for(var j = 0; j < claims.data.length; j++){
             
-            (function processFiles() {
-                                
-                if(files[i] == 'processed' || files[i] == 'skipped' || files[i] == 'zipped') {
-                
-                    increment();
-                
-                }else {
-                
-                    var output = fs.createWriteStream('./claim-data/zipped/' + files[i] + '.zip');
+                filesReady.push(claims.data[j]['Preapproval Number']);
+    
+            }
+            
+            zipFiles();
+            
+        });
 
-                    var archive = archiver('zip', {
-                        zlib: {
-                            level: 9
-                        }
-                    });
-                
-                    output.on('close', function() {
-                                
-                        moveFiles();
-                
-                    });
-                
-                    archive.on('warning', function(err) {
+        function zipFiles(){
+            
+            console.log('Starting zip process.');
+            
+            fs.readdir('./claim-data/', (err, files) => {
 
-                        if (err.code === 'ENOENT') {
-
-                            console.log("ENOENT Warning: " + err);
-
-                        }else {
-
-                            console.log(err);
-
-                        }
-
-                    });
-                
-                    archive.on('error', function(err) {
-
-                        moveFiles(err);
-
-                    });
-                
-                    archive.pipe(output);
-                
-                    archive.directory('./claim-data/' + files[i], '/');
-                
-                    archive.finalize();
-                
-                }
-
-                function moveFiles(err){
-
-                    if(err){
+                (function processFiles() {
                     
-                        console.log(err);
-                        
-                        fs.rename('./claim-data/' + files[i], './claim-data/skipped/' + files[i], function (err) {
-
-                            if(err) console.log(err);
-                            console.log('Skipped ' + files[i]);
-                            increment();
-                            
-                        });
-                        
-                    }else{
-                        
-                        fs.rename('./claim-data/' + files[i], './claim-data/processed/' + files[i], function (err) {
-                            
-                            if(err) console.log(err);
-                            console.log('Processed ' + files[i]);
-                            increment();
-                
-                        });
-                        
-                    }
+                    if(files[i] == 'processed' || files[i] == 'skipped' || files[i] == 'zipped') {
                     
-                }
-
-                function increment() {
-
-                    i++;
+                        increment();
                     
-                    if(i < files.length){
+                    }else if(!filesReady.find(el => {return el == files[i]})) {
 
-                        processFiles();
+                        console.log('skipped');
 
+                        increment();
+    
                     }else {
-
-                        console.log('Finished zipping.');
-                        resolve();
-
+                    
+                        var output = fs.createWriteStream('./claim-data/zipped/' + files[i] + '.zip');
+    
+                        var archive = archiver('zip', {
+                            zlib: {
+                                level: 9
+                            }
+                        });
+                    
+                        output.on('close', function() {
+                                    
+                            moveFiles();
+                    
+                        });
+                    
+                        archive.on('warning', function(err) {
+    
+                            if (err.code === 'ENOENT') {
+    
+                                console.log("ENOENT Warning: " + err);
+    
+                            }else {
+    
+                                console.log(err);
+    
+                            }
+    
+                        });
+                    
+                        archive.on('error', function(err) {
+    
+                            moveFiles(err);
+    
+                        });
+                    
+                        archive.pipe(output);
+                    
+                        archive.directory('./claim-data/' + files[i], '/');
+                    
+                        archive.finalize();
+                    
                     }
+    
+                    function moveFiles(err){
+    
+                        if(err){
+                        
+                            console.log(err);
+                            
+                            fs.rename('./claim-data/' + files[i], './claim-data/skipped/' + files[i], function (err) {
+    
+                                if(err) console.log(err);
+                                console.log('Skipped ' + files[i]);
+                                increment();
+                                
+                            });
+                            
+                        }else{
+                            
+                            fs.rename('./claim-data/' + files[i], './claim-data/processed/' + files[i], function (err) {
+                                
+                                if(err) console.log(err);
+                                console.log('Processed ' + files[i]);
+                                increment();
+                    
+                            });
+                            
+                        }
+                        
+                    }
+    
+                    function increment() {
+    
+                        i++;
+                        
+                        if(i < files.length){
+    
+                            processFiles();
+    
+                        }else {
+    
+                            console.log('Finished zipping.');
+                            resolve();
+    
+                        }
+    
+                    }
+    
+                })();
+    
+            });
 
-                }
+        }
 
-            })();
+    });
+
+    function getClaims(){
+
+        return new Promise(function(resolve, reject){
+
+            fs.readFile('./app/claims.json', 'utf8', function (err, data) {
+
+                if(err) console.log(err);
+    
+                console.log('data: ' + data);
+    
+                resolve(JSON.parse(data));
+                
+            });
 
         });
 
-    });
+    }
 
 }
