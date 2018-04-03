ed1e8b8e087780bbc835081bf6d29ef0f0ce21b8
Austin Gordon <gordon.austin2@gmail.com>
2018-04-03 13:02:04 -0500

    Removed option to enter info directly into claims.json

--- submitClaims.js
+++ submitClaims.js
@@ -2,30 +2,10 @@ require('dotenv').config();
 const app = require('./app/app.js');
 const zipper = require('./app/zipper.js');
 const claimConverter = require('./app/claimConverter.js');
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+claimConverter.run().then(() => {
 
-rl.question('Would you like to convert claims.csv? (y or n)\nType n if you entered claim data in claims.json\n', (answer) => {
-
-    if(answer == 'y'){
-
-        claimConverter.run().then(() => {
-
-            startApp();
-
-        });
-
-    }else {
-
-        startApp();
-
-    }
-
-    rl.close();
+    startApp();
 
 });
 
