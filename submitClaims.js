cb865dca87df8b01f71f6ccd1ac07c408dd7ede4
AustinLeeGordon <gordon.austin2@gmail.com>
2018-12-11 15:36:43 -0600

    Updated media types, removed function that clears claims.csv

--- submitClaims.js
+++ submitClaims.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 const app = require('./app/app.js');
 const zipper = require('./app/zipper.js');
 const claimConverter = require('./app/claimConverter.js');
-var fs = require('fs');
-
-var csvHeaders = "Media Type,Invoice Amount,Media Name,Invoice Number,Invoice Date,Preapproval Number,Nested";
 
 claimConverter.run().then(() => {
 
@@ -23,14 +20,7 @@ function startApp() {
                 process.exit(0);
             }
 
-            fs.writeFile('./app/claims.csv', csvHeaders, err => {
-
-                if(err) console.log(err);
-
-                console.log('Reset claims.csv');
-                console.log('Uploading completed');
-
-            });
+            console.log('Uploading completed');
         
         });
     
